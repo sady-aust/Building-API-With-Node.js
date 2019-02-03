0b3498236882760da011d65c71197f0ff3579fbf
Md. Toufiqul Islam <sadydic@gmail.com>
2019-02-03 21:02:19 +0600

    conenction with DB,Insert Data

--- api/routes/product.js
+++ api/routes/product.js
@@ -1,5 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const mysql = require('mysql');
+
+var mysqlConnection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password:"",
+    database:"myrestapi"
+});
+
+mysqlConnection.connect((err)=>{
+    if(!err){
+        console.log("DB COnnection Succeed");
+        
+    }
+    else{
+        console.log("DB Connection Failed "+JSON.stringify(err,undefined,2));
+        
+    }
+});
 
 router.get("/",(req,res,next)=>{
     res.status(200).json({
@@ -8,9 +27,25 @@ router.get("/",(req,res,next)=>{
 });
 
 router.post("/",(req,res,next)=>{
-    res.status(201).json({
-        message: 'Handling Post requst to /products'
+    const product = {
+        name:req.body.name,
+        price:req.body.price
+    };
+
+    let sql = "INSERT INTO products(name,price) VALUES('"+product.name+"','"+product.price+"')";
+    mysqlConnection.query(sql,(err,rows,field)=>{
+        if(err){
+            res.status(200).json({
+                message: 'Not Inserted',
+            });
+        }
+        else{
+            res.status(200).json({
+                message: 'Inserted Successfully',
+            });
+        }
     });
+   
 });
 
 router.get("/:productId",(req,res,next)=>{
--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan')
+const bodyParser = require('body-parser');
 
 const productRoutes = require("./api/routes/product")
 const orderRouter = require('./api/routes/order');
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json())
+
+//For handling CORL Error
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+  });
+  
 
 //routes which should handle the request
 app.use("/products",productRoutes);
